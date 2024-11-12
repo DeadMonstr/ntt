f2c8c18f112400c5f3d75fb1aa80aee4fbafce38
DeadMonstr <deedmonstr@gmail.com>
2024-11-12 21:17:26 +0500

    add new files

---
+++ src/app/routers/config/routersConfig.js
@@ -0,0 +1,13 @@
+import {getRouteMain} from "shared/const/routers";
+
+
+export const routersConfig = [
+    {
+        name: "Bosh Sahifa",
+        path: getRouteMain(),
+        // element: <HomePage/>,
+        element: null,
+    },
+
+
+]
