60a8168354df623e509c6a97a18613265877cef4
ikromovvv <isardor859@gmail.com>
2024-11-16 15:43:40 +0300

    dsda

--- src/entities/application/ui/applicationList/applicationList.js
+++ src/entities/application/ui/applicationList/applicationList.js
@@ -8,6 +8,7 @@ import {memo, useEffect, useState} from "react";
 import {Table} from "shared/ui/table";
 
 import cls from "./applicationList.module.sass";
+import {useNavigate} from "react-router";
 
 export const ApplicationList = memo(() => {
 
@@ -18,6 +19,8 @@ export const ApplicationList = memo(() => {
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
     }, []);
+
+    const navigate = useNavigate()
     return (
         <div className={cls.applicationList}>
             <Table>
@@ -27,156 +30,48 @@ export const ApplicationList = memo(() => {
                     <th>Ism sharif</th>
                     <th>Telefon raqam</th>
                     <th>Daraja</th>
-                    <th>Ta’lim yo’nalishlari</th>
-                    <th>Ta’lim shakli</th>
-                    <th>Ta’lim tili</th>
-                    <th>Topshirilgan sana</th>
+                    {!isMobile ? <>
+                        <th>Ta’lim yo’nalishlari</th>
+                        <th>Ta’lim shakli</th>
+                        <th>Ta’lim tili</th>
+                        <th>Topshirilgan sana</th>
+                    </> : null}
+
                 </tr>
                 </thead>
                 <tbody>
                 <tr
+                    onClick={() => navigate("profile")}
                     className={classNames(cls.applicationList__list, {
                         [cls.debt]: true
                     })}
                 >
                     <td/>
                     <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
+                    <td>+998 911234567</td>
                     <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
+                    {!isMobile ? <>
+                        <td>Matematika</td>
+                        <td>Sirtqi</td>
+                        <td>O'zbek tili</td>
+                        <td>13.07.2024</td>
+                    </> : null}
                 </tr>
-                <tr className={cls.applicationList__list}>
+                <tr
+                    onClick={() => navigate("profile")}
+                    className={cls.applicationList__list}
+                >
                     <td/>
                     <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
+                    <td>+998 911234567</td>
                     <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
-                </tr>
-                <tr className={cls.applicationList__list}>
-                    <td/>
-                    <td>Quddusbek Azzamov Aminjonovich</td>
-                    <td>+998 91 123 45 67</td>
-                    <td>Bakalavr</td>
-                    <td>Matematika</td>
-                    <td>Sirtqi</td>
-                    <td>O'zbek tili</td>
-                    <td>13.07.2024</td>
+                    {!isMobile ? <>
+
+                        <td>Matematika</td>
+                        <td>Sirtqi</td>
+                        <td>O'zbek tili</td>
+                        <td>13.07.2024</td>
+                    </> : null}
                 </tr>
                 </tbody>
             </Table>
