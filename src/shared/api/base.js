97fd275a0afbd62fb3006399216b91bca1df15ce
shaha <shahaunarov7@gmail.com>
2025-01-22 20:45:54 +0500

    for test

--- src/shared/api/base.js
+++ src/shared/api/base.js
@@ -1,6 +1,6 @@
 
 
-export const API_URL_DOC = `http://26.48.191.92:8000/`
+export const API_URL_DOC = `http://26.196.249.247:8000/`
 export const API_URL = `${API_URL_DOC}api/`
 export const CLASSROOM_API_URL = `http://localhost:3000/`
 export const CLASSROOM_API_URL_DOC = `http://192.168.68.116:8000/`
