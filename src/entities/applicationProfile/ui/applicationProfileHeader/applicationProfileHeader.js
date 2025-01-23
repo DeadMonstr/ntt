dc04b48083f205f594916ab3d25a182bf12d289b
ikromovvv <isardor859@gmail.com>
2025-01-23 12:49:29 +0300

    pushher

--- src/entities/applicationProfile/ui/applicationProfileHeader/applicationProfileHeader.js
+++ src/entities/applicationProfile/ui/applicationProfileHeader/applicationProfileHeader.js
@@ -11,7 +11,7 @@ export const ApplicationProfileHeader = () => {
 
     return (
         <div className={`${cls.application__header} ${cls.application__box}`}>
-            <div onClick={() => navigate(-1)}>r
+            <div onClick={() => navigate(-1)}>
                 <i className="fas fa-arrow-left"/>
             </div>
             <div>
